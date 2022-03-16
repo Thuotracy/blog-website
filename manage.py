0e091b5b7c397b584185b114f596e85c925d561f
tracy <tracyjacobs775@gmail.com>
2022-03-16 22:33:15 +0300

    add fonts

--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-# from app import  db,create_app
+from app import  db,create_app
 from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 from app.models import Blogpost
