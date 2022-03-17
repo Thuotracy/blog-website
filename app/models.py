ef1c4c707eed5a0ed937f92eedb71b5cc515e301
tracy <tracyjacobs775@gmail.com>
2022-03-17 16:09:16 +0300

    final commit

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from flask_login import UserMixin
 
 class Blogpost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -7,3 +8,23 @@ class Blogpost(db.Model):
     author = db.Column(db.String(20))
     date_posted = db.Column(db.DateTime)
     content = db.Column(db.Text)
+
+class User(UserMixin,db.Model):
+    '''
+    class that handles the user infomation
+    '''
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    password= db.Column(db.String(255))
+    def __init__(self, username,email,password):
+        self.username= username
+        self.email=email
+        self.password=password   
+
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+        
--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
-from flask import render_template,url_for,Blueprint,request,redirect
+from flask import render_template,url_for,Blueprint,request,redirect,flash,session
 from datetime import datetime
-from .models import Blogpost
+from .models import Blogpost, User
 from app import db
+from .forms import RegistrationForm
+from werkzeug.security import generate_password_hash,check_password_hash 
 
 views = Blueprint('views',__name__)
 
@@ -10,6 +12,61 @@ def index():
     posts = Blogpost.query.order_by(Blogpost.date_posted.desc()).all()
     
     return render_template('index.html', posts=posts)
+
+@views.route("/register")
+def register():
+    Registration= RegistrationForm()
+
+    return render_template('register.html', Registration=RegistrationForm)
+
+@views.route('/success', methods=['GET','POST'])
+def success():
+    if request.method == "POST":
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password1 = request.form.get('psw1')
+        password2 = request.form.get('psw2')
+        user_info =db.session.query(User).filter(User.email ==email)
+        email_exist=User.query.filter_by(email =email).first()
+        username_exist = User.query.filter_by(username =username).first()
+        if email_exist:
+            flash('User with that email address already exist', category='error')
+            return render_template('form.html', text ='User with that email address already exist')
+        elif username_exist:
+            flash('User name already in use', category='error')
+            return render_template('form.html', text ='User name already in use.')
+        elif password1 != password2 :
+            flash( 'Password dont\'t match' , category='error')
+            return render_template('form.html', text ='Password don\'t match!')
+        elif len(password1) < 6:
+            flash('Password is too short', category='error')
+            return render_template('form.html', text ='password too short. use at least 6 characters')
+        else:
+            data = User(username,email,password=generate_password_hash(password1, method='sha256'))
+            db.session.add(data)
+            db.session.commit()
+            # logout_user(data)
+            
+            return render_template('success.html')
+
+@views.route('/login' )
+def login():
+    return render_template('login.html')
+
+    
+@views.route('/profile', methods=['GET','POST'])
+def profile():
+    if request.method == "POST":
+        email = request.form.get('email')
+        password = request.form.get('psw')
+        user = User.query.filter_by(email=email).first()
+        if not user or not check_password_hash(user.password, password):
+            text='Please check your login details and try again.'
+            return render_template('login.html', text=text)
+        session['email']=user.email
+        name = user.username
+        # login_user(user, remember= True)
+        return render_template('profile.html',user=user)
 
 @views.route('/about')
 def about():
